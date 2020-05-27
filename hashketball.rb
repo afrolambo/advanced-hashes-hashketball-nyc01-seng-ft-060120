db623d0790f4644953bed4a1358fdbd1e7a68d3d
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-27 02:13:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -203,7 +203,7 @@ def big_shoe_rebounds
     team_info[:players].each do |info|
       until big_shoe > info[:shoe] do 
         big_shoe = info[:shoe] 
-      return info
+      p info
     rebound = info[:rebounds]
       return rebound
       end
